61259e7a9557fcd6205695ec5b4736975e5e5745
changlehung <changlehung@qq.com>
2023-11-21 00:42:49 +0800

    udpate BoolLiteral

--- src/main/java/org/spl/compiler/ir/vals/BoolLiteral.java
+++ src/main/java/org/spl/compiler/ir/vals/BoolLiteral.java
@@ -6,35 +6,25 @@ import org.spl.compiler.ir.ASTContext;
 import org.spl.compiler.ir.IRNode;
 import org.spl.compiler.ir.Op;
 
-public class BoolLiteral implements IRNode<Instruction> {
-
-  private final byte oparg;
-
-  public BoolLiteral(byte oparg) {
-    this.oparg = oparg;
-  }
+public record BoolLiteral(Object val, byte oparg) implements IRNode<Instruction> {
 
   @Override
   public void codeGen(ASTContext<Instruction> context) {
     context.add(new Instruction(OpCode.LOAD_CONST, oparg));
   }
 
-  public boolean getVal() {
-    return oparg == 0; // 0 means true and 1 means false
-  }
-
   @Override
   public boolean isLiteral() {
-    return IRNode.super.isLiteral();
+    return true;
   }
 
   @Override
   public boolean isBooleanLiteral() {
-    return IRNode.super.isBooleanLiteral();
+    return true;
   }
 
   @Override
   public Op getOperator() {
-    return IRNode.super.getOperator();
+    return Op.NOP;
   }
 }
