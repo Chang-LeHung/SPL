bb2e4ed402c1bc5c24bfe5b5d713ae2aff01a500
changlehung <changlehung@qq.com>
2023-12-08 01:20:04 +0800

    update builtin names

--- src/main/java/org/spl/compiler/ir/BuiltinNames.java
+++ src/main/java/org/spl/compiler/ir/BuiltinNames.java
@@ -5,10 +5,27 @@ import java.util.Set;
 
 public class BuiltinNames {
 
-  public static Set<String> builtinNames = new HashSet<String>();
+  public static Set<String> builtinNames = new HashSet<>();
 
   static {
     builtinNames.add("print");
+    builtinNames.add("max");
+    builtinNames.add("min");
+    builtinNames.add("abs");
+    builtinNames.add("pow");
+    builtinNames.add("sqrt");
+    builtinNames.add("sin");
+    builtinNames.add("cos");
+    builtinNames.add("tan");
+    builtinNames.add("asin");
+    builtinNames.add("acos");
+    builtinNames.add("atan");
+    builtinNames.add("log");
+    builtinNames.add("log10");
+    builtinNames.add("exp");
+    builtinNames.add("floor");
+    builtinNames.add("ceil");
+    builtinNames.add("round");
   }
 
   public static void add(String name) {
