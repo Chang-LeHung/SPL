ceadb308d2dc07137c5fe78026ec20e79842e108
changlehung <changlehung@qq.com>
2023-12-09 21:08:03 +0800

    update SPLNoneObject method __str__

--- src/main/java/org/spl/vm/objects/SPLNoneObject.java
+++ src/main/java/org/spl/vm/objects/SPLNoneObject.java
@@ -1,9 +1,11 @@
 package org.spl.vm.objects;
 
+import org.spl.vm.exceptions.jexceptions.SPLInternalException;
 import org.spl.vm.types.SPLCommonType;
 import org.spl.vm.types.SPLNoneType;
 
 public class SPLNoneObject extends SPLObject{
+  public static SPLStringObject name = new SPLStringObject("None");
   private SPLNoneObject(SPLCommonType type) {
     super(type);
   }
@@ -14,5 +16,20 @@ public class SPLNoneObject extends SPLObject{
 
   public static SPLNoneObject getInstance() {
     return SingletonHolder.INSTANCE;
+  }
+
+  @Override
+  public SPLObject __eq__(SPLObject rhs) throws SPLInternalException {
+    return this == rhs ? SPLBoolObject.getTrue() :  SPLBoolObject.getFalse();
+  }
+
+  @Override
+  public SPLObject __ne__(SPLObject rhs) throws SPLInternalException {
+    return this == rhs ? SPLBoolObject.getFalse() :  SPLBoolObject.getTrue();
+  }
+
+  @Override
+  public SPLObject __str__() {
+    return name;
   }
 }
