3e7b5ad3ec3c6cd5f9e266f9c687473b7fcfb536
changlehung <changlehung@qq.com>
2023-12-11 16:18:07 +0800

    add [] operator and mehtod slice

--- src/main/java/org/spl/vm/objects/SPLStringObject.java
+++ src/main/java/org/spl/vm/objects/SPLStringObject.java
@@ -2,9 +2,12 @@ package org.spl.vm.objects;
 
 import org.spl.vm.exceptions.SPLErrorUtils;
 import org.spl.vm.exceptions.jexceptions.SPLInternalException;
+import org.spl.vm.exceptions.splexceptions.SPLOutOfBoundException;
+import org.spl.vm.exceptions.splexceptions.SPLRuntimeException;
 import org.spl.vm.exceptions.splexceptions.SPLTypeError;
 import org.spl.vm.types.SPLStringType;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class SPLStringObject extends SPLObject {
@@ -103,5 +106,49 @@ public class SPLStringObject extends SPLObject {
     if (this == o) return true;
     if (!(o instanceof SPLStringObject that)) return false;
     return Objects.equals(msg, that.msg);
+  }
+
+  @Override
+  public SPLObject __getIterator__() throws SPLInternalException {
+    ArrayList<SPLStringObject> subs = new ArrayList<>();
+    for (int i = 0; i < msg.length(); i++) {
+      subs.add(new SPLStringObject(msg.substring(i, i + 1)));
+    }
+    return new SPLCommonIterator(subs);
+  }
+
+  @Override
+  public SPLObject __subscribe__(SPLObject args) throws SPLInternalException {
+    if (args instanceof SPLLongObject l) {
+      if (l.getVal() < msg.length())
+        return new SPLStringObject(msg.substring((int) l.getVal(), (int) l.getVal() + 1));
+      else
+        return SPLErrorUtils.splErrorFormat(new SPLOutOfBoundException("index out of range"));
+    }
+    return SPLErrorUtils.splErrorFormat(new SPLTypeError("can not apply operator '[]' on str and " + args.getType().getName()));
+  }
+
+  public SPLObject slice(SPLObject... args) throws SPLInternalException {
+    if (args.length == 2) {
+      SPLObject L = args[0];
+      SPLObject R = args[1];
+      if (L instanceof SPLLongObject l && R instanceof SPLLongObject r) {
+        if (l.getVal() < msg.length() && r.getVal() < msg.length()) {
+          return new SPLStringObject(msg.substring((int) l.getVal(), (int) r.getVal() + 1));
+        }
+      }
+    } else if (args.length == 3) {
+      SPLObject L = args[0];
+      SPLObject R = args[1];
+      SPLObject S = args[2];
+      if (L instanceof SPLLongObject l && R instanceof SPLLongObject r && S instanceof SPLLongObject s) {
+        StringBuilder builder = new StringBuilder();
+        for(long i = l.getVal(); i < r.getVal(); i += s.getVal()) {
+          builder.append(msg.charAt((int) i));
+        }
+        return new SPLStringObject(builder.toString());
+      }
+    }
+    return SPLErrorUtils.splErrorFormat(new SPLRuntimeException("Invalid arguments for method 'slice'"));
   }
 }
